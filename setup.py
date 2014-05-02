7ac8c7cf75b0f8caf736847a26a7bdbd2c3f5ae2
Wolfgang Richter <wolf@cs.cmu.edu>
2014-05-02 00:43:53 -0400

    setup.py: new release, bumping PATCH version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ from setuptools import setup
 
 setup(
     name='TermRecord',
-    version='1.1.2',
+    version='1.1.3',
     url='http://github.com/theonewolf/TermRecord',
     license='MIT',
     author='Wolfgang Richter',
